a84d517ccdc68cb968d351a2defd9f5f9e85554e
Liam Friel <liam.friel@gmail.com>
2015-02-17 22:48:57 +0000

    tidy up

--- jabber/manifests/init.pp
+++ jabber/manifests/init.pp
@@ -9,116 +9,12 @@ class jabber() {
 
     # Some definitions
     include jabber::params
+    
+    # Do build setup ... 
+    include jabber::setup
 
-    # Create the jabber group and user
-    group { "${jabber_group}":
-            ensure => present,
-            gid    => 2000
-    }
-    user { "${jabber_user}":
-           ensure => present,
-           gid    => "${jabber_group}",
-           membership => minimum,
-           shell      => "/bin/bash",
-           require    => Group["${jabber_group}"]
-    }
-    
-    # Create directories for the PID and logs and set them to be owned by Jabber
-    file { $jabber_pid_dir:
-           ensure  => "directory",
-           owner   => "jabber",
-           group   => "jabber",
-           mode    => 750,
-           require => User["${jabber_user}"]
-    }
-   
-    file { $jabber_log_dir:
-           ensure  => "directory",
-           owner   => "jabber",
-           group   => "jabber",
-           mode    => 750,
-           require => User["${jabber_user}"]
-    }
-    
-    # Create the source directory
-    file { "${src_dir}":
-            ensure => "directory",
-            mode => 777
-    }
-    
-    ->
-    
-    # Fetch the jabber source archive  
-    wget::fetch { "Fetch Jabber source archive to ${src_dir}/${jabber_archive}":
-                   source => "https://github.com/jabberd2/jabberd2/releases/download/jabberd-2.3.2/${jabber_archive}",
-                   destination => "${src_dir}/${jabber_archive}",
-                   verbose => true
-    }
-    
-    ->
-    
-    # Unpack the archives 
-   
-    exec { "unpack jabber archive to ${src_dir}/${jabber_vers}":
-        user    => $user,
-        cwd     => $src_dir,
-        timeout => $timeout,
-        path    => $path,
-        command => "tar xvfz ${jabber_archive}",
-        creates => "${src_dir}/${jabber_vers}/configure"
-    }
-    
-    # From our current PostgreSQL setup, the pg_config utility is not in the location
-    # expected by the configure script: so copy it
-    
-    ->
-    
-    exec { "copy pg_config utility":
-        user    => $user,
-        cwd     => "/usr/bin",
-        timeout => $timeout,
-        path    => $path,
-        command => "cp ${pg_config_util} ${pg_config_dir}/pg_config",
-        creates => "${pg_config_dir}/pg_config"
-        
-    }
-    
-    # Run the configure command (which generates the Makefile)
-    
-    ->
-    
-    exec { "configure jabber before building":
-        user    => $user,
-        cwd     => "${src_dir}/${jabber_vers}",
-        timeout => $timeout,
-        path    => $path,
-        command => "${src_dir}/${jabber_vers}/configure --enable-pgsql=/usr/bin/pg_config --enable-ssl=/usr/bin/openssl --enable-idn",
-        creates => "${src_dir}/${jabber_vers}/Makefile"
-    }    
-
-    # Now build the jabber components and install it
-    
-    ->
-    
-    exec { "build the jabber components":
-        user    => $user,
-        cwd     => "${src_dir}/${jabber_vers}",
-        timeout => $timeout,
-        path    => $path,
-        command => "make",
-        creates => "${src_dir}/${jabber_vers}/c2s"
-    }
-    
-    ->
-    
-    exec { "install the jabber components":
-        user    => $user,
-        cwd     => "${src_dir}/${jabber_vers}",
-        timeout => $timeout,
-        path    => $path,
-        command => "make install",
-        creates => "${jabber_comp_inst}/jabberd.service"
-    }
+    # Build and Install Jabber components
+    include jabber::build    
     
     
 }
